5fc8c93d2678c039b153734f99dc39e52ff64f46
L4P <209110931+LoveForPeeps@users.noreply.github.com>
2025-08-02 23:15:25 +0530

    Moved drawing loop to a named function with a size parameter defaulting to 16

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const sketchContainer = document.querySelector("#sketch-container")
 
-for (let i = 0; i < 16; i++) {
-    const squareRow = document.createElement('div')
+function drawSquares(size = 16) {
+    for (let i = 0; i < size; i++) {
+        const squareRow = document.createElement('div')
 
-    for (let i = 0; i < 16; i++) {
-        const newSquare = document.createElement('div')
-        squareRow.appendChild(newSquare)
+        for (let i = 0; i < size; i++) {
+            const newSquare = document.createElement('div')
+            squareRow.appendChild(newSquare)
+        }
+
+        sketchContainer.appendChild(squareRow)
     }
-   
-    sketchContainer.appendChild(squareRow)
 }
+
+drawSquares();

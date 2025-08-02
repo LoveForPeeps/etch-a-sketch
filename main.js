675ff97ef867a3555b905c4fffcbbef43934cfe5
L4P <209110931+LoveForPeeps@users.noreply.github.com>
2025-08-02 23:39:37 +0530

    Added redrawSquares() that clears the current squares and prompts the user for the size.

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const sketchContainer = document.querySelector("#sketch-container")
 
+function promptUser() {
+    return prompt("Input a number (less than 100)", 16);
+}
+
 function drawSquares(size = 16) {
     for (let i = 0; i < size; i++) {
-        const squareRow = document.createElement('div')
+        const squareRow = document.createElement('div');
 
         for (let i = 0; i < size; i++) {
-            const newSquare = document.createElement('div')
-            squareRow.appendChild(newSquare)
+            const newSquare = document.createElement('div');
+            squareRow.appendChild(newSquare);
         }
 
-        sketchContainer.appendChild(squareRow)
+        sketchContainer.appendChild(squareRow);
     }
 }
 
-drawSquares();
+function redrawSquares(size) {
+    if (size === null) {
+        return;
+    }
+
+    const parsedNumber = parseInt(size)
+    if (isNaN(parsedNumber) || typeof(parsedNumber) != 'number' || parsedNumber > 100 || parsedNumber <= 0) {
+        redrawSquares(promptUser());
+    }
+
+    sketchContainer.innerHTML = ""; // Clear all ChildNodes
+
+    drawSquares(size);
+}
+
+drawSquares(16);
